9a33c8fade389c5cafbf9e7e6e235666dd00f37a
coachripper@gmail.com <coachripper@gmail.com>
2022-03-30 15:37:01 +0000

    sf cust purch

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -5,24 +5,23 @@
  
  Try changing "table" to "view" below
  */
-{ { config(materialized = 'table') } } with source_data as (
-    SELECT
-        c.c_custkey as customer,
-        c.c_name as customer_name,
-        c.c_nationkey as nation,
-        SUM(o.o_totalprice) as total_order_price
-    FROM
-        SNOWFLAKE_SAMPLE_DATA.TPCH_SF1.CUSTOMER c
-        LEFT JOIN SNOWFLAKE_SAMPLE_DATA.TPCH_SF1.ORDERS o ON c.c_custkey = o.o_custkey
-    GROUP BY
-        c.c_custkey,
-        c.c_name,
-        c.c_nationkey
+{{ config(materialized='table') }}
+
+
+with source_data as (
+
+    select 1 as id
+
+    union all
+
+    select null as id
+
 )
-select
-    *
-from
-    source_data
+
+select *
+
+from source_data
+
     /*
      Uncomment the line below to remove records with null `id` values
      */
